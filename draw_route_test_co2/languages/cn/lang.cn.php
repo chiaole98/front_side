346a1e033c42fdb0e16dffd28abe674ba554232e
chiaole98 <xk4871225@gmail.com>
2020-09-20 18:29:16 +0800

    0920-2

---
+++ draw_route_test_co2/languages/cn/lang.cn.php
@@ -0,0 +1,35 @@
+<?php
+$lang['username'] = '使用者名稱';
+$lang['enter_username'] = '輸入使用者名稱';
+$lang['total_distance']  = '總距離';
+$lang['index_title']  = '計算兩個地址之間的距離';
+$lang['origin']  = '起點';
+$lang['destination']  = '終點';
+$lang['enter_location']  = '輸入地點';
+$lang['calculate_co2'] = '碳排放量為';
+$lang['distance_in_kilo'] = '公里距離';
+$lang['in_text'] = '花費時間';
+$lang['from'] = '從';
+$lang['to'] = '到';
+$lang['calculate_btn']  = '計算';
+$lang['Driving'] = '開車';
+$lang['Walking'] = '走路';
+$lang['Bicycling'] = '騎腳踏車';
+$lang['Transit'] = '大眾運輸工具';
+$lang['travel_mode']  = '選擇交通工具';
+$lang['taxi']  = '計程車';
+$lang['foot']  = '步行';
+$lang['moto']  = '騎車';
+$lang['bic']  = '騎腳踏車';
+$lang['transit']  = '大眾運輸工具';
+$lang['tram']  = '電車';
+$lang['home'] = '主頁';
+$lang['about'] = '關於';
+$lang['products'] = '產品';
+$lang['services'] = '服務';
+$lang['contact_us'] = '聯絡我們';
+$lang['train']  = '火車';
+$lang['gram']  = '公克';
+$lang['menu_message']  = '使用GIS計算CO<sub>2排放量</sub> 從運輸';
+
+
